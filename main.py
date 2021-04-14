eb25f998798a38a2980887d69dfd41a1eea52dca
lzyscript <953560328@qq.com>
2021-04-14 13:44:09 +0800

    list

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import redis
 
 
 rdb = redis.Redis(host='localhost', port=6379, db=0)
+
+
+if __name__ == "__main__":
+	if rdb.ping():
+		print("redis connected!")
+	else:
+		print("redis not connected!")
